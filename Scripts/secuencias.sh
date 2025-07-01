c5ad114dc9fd66df1cb1c1326c5ad7fc40f27188
LuzuLiz <luzuriagaliz@gmail.com>
2025-07-01 17:14:34 -0500

    secuencias.sh

--- Scripts/secuencias.sh
+++ Scripts/secuencias.sh
@@ -1,8 +1,3 @@
-#Proximos pasos:
--Descargar la secuencia del marcador SRS57
--Buscar otras secuencias del gen SAG3 reportadas en Ecuador
--Alinearlas las secuencias para evaluar diversidad genetica
-
 1. Secuencia marcador SR57, buscar coordenadas del gen
 grep -i 'SRS57' genomic.gff
 
@@ -12,4 +7,8 @@ samtools faidx GCF_000006565.2_TGA4_genomic.fna
 3.Extraer fragmento genomico marcador SRS57 (hebra negativa)
 samtools faidx GCF_000006565.2_TGA4_genomic.fna NC_031480.1:508546-512330 > SRS57_gen.fasta
 
+4. Extraer CDS (secuencia codificante)
+samtools faidx GCF_000006565.2_TGA4_genomic.fna NC_031480.1:510586-511743 > SRS57_cds.fasta
+
+5. Secuencias SRS57 en GenBank reportadas en Ecuador
 
