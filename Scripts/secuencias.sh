21140cb9bd4b56b965c0a7e5095fa18ac3102a6a
LuzuLiz <luzuriagaliz@gmail.com>
2025-07-02 08:14:13 -0500

    secuencias.sh

--- Scripts/secuencias.sh
+++ Scripts/secuencias.sh
@@ -4,7 +4,7 @@ grep -i 'SRS57' genomic.gff
 #2. Extraer secuencia completa con samtools
 samtools faidx GCF_000006565.2_TGA4_genomic.fna 
 
-#3.Extraer fragmento genomico marcador SRS57 (hebra negativa)
+#3.Extraer fragmento genomico marcador SRS57 (hebra negativa) (¿Se debe convertir?)
 samtools faidx GCF_000006565.2_TGA4_genomic.fna NC_031480.1:508546-512330 > SRS57_gen.fasta
 
 #4. Extraer CDS (secuencia codificante)
