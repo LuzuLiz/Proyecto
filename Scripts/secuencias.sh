9964ba3ec6bf66c230330b63f76477c4fad81501
LuzuLiz <luzuriagaliz@gmail.com>
2025-07-09 17:30:01 -0500

    secuencias.sh

--- Scripts/secuencias.sh
+++ Scripts/secuencias.sh
@@ -11,16 +11,9 @@ samtools faidx GCF_000006565.2_TGA4_genomic.fna NC_031480.1:508546-512330 > SRS5
 samtools faidx GCF_000006565.2_TGA4_genomic.fna NC_031480.1:510586-511743 > SRS57_cds.fasta
 
 #5. Descargar secuencias reportadas en America Latina
-esearch -db nucleotide -query "Toxoplasma gondii[Organism] AND SAG3[Gene] AND (Brazil OR Argentina OR Colombia OR Ecuador OR GUYANA)" | efetch -format acc > sga3_latam.txt
+esearch -db nucleotide -query "Toxoplasma gondii[Organism] AND SAG3[Gene] AND (Brazil OR Colombia OR Mexico )" | efetch -format acc > sga3_latam.txt
 
-#6. Descargar las secuencias en archivos Fasta
-cat sga3_latam.txt | xargs -n 1 -I {} efetch -db nucleotide -id {} -format fasta > sga3_latam.fasta
-
-#7. Alineamiento de secuencias con muscle
-./muscle3.8.31_i86linux64 -in sga3_latam.fasta -out sga3_aligned.fasta
-
-#8. Construccion de filogenia secuencias 
--iqtree -s sga3_aligned.fasta
+#6. 
 
 
 ------
